facc531fa4c0577672296d06a3d43c0efff816e6
akeem-s <akeemstreet@gmail.com>
2016-07-09 09:59:05 -0400

    Add input route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'phones/input', :to => 'phones#input'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
