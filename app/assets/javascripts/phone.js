5a5f34764c234c3bba8c8e2a9e1098bd419bd355
akeem-s <akeemstreet@gmail.com>
2016-07-20 00:41:38 -0400

    Site is functional

--- app/assets/javascripts/phone.js
+++ app/assets/javascripts/phone.js
@@ -1,6 +1,14 @@
-// $(document).ready(function(){
-//   $("#phone-form").on("submit", function(event){
-//     event.preventDefault();
-//     alert("hit the preventDefault")
-//   })
-// })
+$(document).ready(function(){
+  $("#phone-form").on("submit", function(event){
+    event.preventDefault();
+    
+    $.ajax({
+      type: "POST",
+      url: "/phones/call",
+      data: $(event.target).serialize(),
+      dataType: "JSON"
+    }).fail(function(response){
+      $('#phone-form')[0].reset();
+    });
+  })
+})
